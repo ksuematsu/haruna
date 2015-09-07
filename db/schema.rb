9178f5ab83d8755723c132ae8ec9b6f6f5b46978
ksuematsu <ksuematsu68@gmail.com>
2015-09-07 16:30:13 -0700

    delete avatar column from user

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150907045341) do
 
   create_table "users", force: :cascade do |t|
     t.string   "user_name",              limit: 255,              null: false
-    t.string   "avatar",                 limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
