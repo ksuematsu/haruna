770d0a9629ab08d5e7d39b1d4c52936b54538db9
ksuematsu <ksuematsu68@gmail.com>
2015-09-07 19:39:07 -0700

    memo todo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+   # TODO: タブ内の詳細は未着手。問題とか作った後にタブのJS化も含めて実装
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
